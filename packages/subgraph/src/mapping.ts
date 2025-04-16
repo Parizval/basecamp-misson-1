7e036b0903f36caa830f4d446dad8130114b8d18
parizval <anmolgoyal@gmail.com>
2025-04-16 16:38:15 +0530

    solved subgraph module

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,4 +1,16 @@
 import { Enlisted as EnlistedEvent } from "../generated/Enlist/Enlist";
 import { Enlisted } from "../generated/schema";
 
-export function handleEnlisted(event: EnlistedEvent): void {}
+export function handleEnlisted(event: EnlistedEvent): void {
+  let id = event.transaction.hash;
+  let enlisted = new Enlisted(id);
+
+  enlisted.account = event.params.source;
+
+  enlisted.blockNumber = event.block.number;
+  enlisted.blockTimestamp = event.block.timestamp;
+
+  enlisted.transactionHash = id;
+
+  enlisted.save();
+}
